1c4bc9f4118f41137c6caa4a79a621b3e7d76a31
Fabio Teixeia <fabio1079@gmail.com>
2014-02-09 12:52:58 -0200

    Separating logic from stage to main

---
+++ main.py
@@ -0,0 +1,26 @@
+import gtk
+import stage
+
+BLOCK_TEMP_TOP = 40.0
+BLOCK_TEMP_RIGHT = 30.0
+BLOCK_TEMP_BOTTOM = 20.0
+BLOCK_TEMP_LEFT = 10.0
+
+def main():
+  window = gtk.Window()
+  window.set_size_request(800, 640)
+
+  stg = stage.Stage(
+    max(BLOCK_TEMP_TOP, BLOCK_TEMP_RIGHT, BLOCK_TEMP_BOTTOM, BLOCK_TEMP_LEFT)/64.0,
+    [BLOCK_TEMP_TOP, BLOCK_TEMP_RIGHT, BLOCK_TEMP_BOTTOM, BLOCK_TEMP_LEFT]
+  )
+  window.add(stg)
+
+  window.connect("destroy", gtk.main_quit)
+  window.show_all()
+  gtk.main()
+
+
+
+if __name__ == "__main__":
+  main()
--- stage.py
+++ stage.py
@@ -6,17 +6,15 @@ TOTAL_LINES = 100
 TOTAL_COLUMNS = 80
 BLOCK_WD = 8
 BLOCK_HG = 8
-BLOCK_TEMP_TOP = 40.0
-BLOCK_TEMP_RIGHT = 30.0
-BLOCK_TEMP_BOTTOM = 20.0
-BLOCK_TEMP_LEFT = 10.0
 
 class Stage(gtk.DrawingArea):
-  def __init__(self, color_grade):
+  def __init__(self, color_grade, side_temp):
     gtk.DrawingArea.__init__(self)
 
     self.color_grade = color_grade
     self.blocks = []
+    self.side_temp = side_temp
+
     self.connect("expose_event", self.expose)
 
   def expose(self, widget, event):
@@ -36,10 +34,10 @@ class Stage(gtk.DrawingArea):
     self.blocks = blocks.generate_block_list(
             TOTAL_LINES, 
             TOTAL_COLUMNS, 
-            BLOCK_TEMP_TOP, 
-            BLOCK_TEMP_RIGHT, 
-            BLOCK_TEMP_BOTTOM, 
-            BLOCK_TEMP_LEFT,
+            self.side_temp[0],
+            self.side_temp[1], 
+            self.side_temp[2], 
+            self.side_temp[3],
             BLOCK_WD, 
             BLOCK_HG
           )
@@ -65,15 +63,3 @@ class Stage(gtk.DrawingArea):
           self.draw(self.blocks[i][j])
 
     gtk.timeout_add(1, self.calculate) # 1000/60
-
-
-if __name__ == "__main__":
-  window = gtk.Window()
-  window.set_size_request(800, 640)
-
-  stg = Stage(max(BLOCK_TEMP_TOP, BLOCK_TEMP_RIGHT, BLOCK_TEMP_BOTTOM, BLOCK_TEMP_LEFT)/64.0)
-  window.add(stg)
-
-  window.connect("destroy", gtk.main_quit)
-  window.show_all()
-  gtk.main()
