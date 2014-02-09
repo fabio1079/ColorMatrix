9599ff3d8c257388c00c3eac536f4835aea79d89
Fabio Teixeia <fabio1079@gmail.com>
2014-02-09 12:44:34 -0200

    Drawing temperature gradient

--- stage.py
+++ stage.py
@@ -2,10 +2,10 @@ import gtk
 import blocks
 import colors
 
-TOTAL_LINES = 80
-TOTAL_COLUMNS = 60
-BLOCK_WD = 10
-BLOCK_HG = 10
+TOTAL_LINES = 100
+TOTAL_COLUMNS = 80
+BLOCK_WD = 8
+BLOCK_HG = 8
 BLOCK_TEMP_TOP = 40.0
 BLOCK_TEMP_RIGHT = 30.0
 BLOCK_TEMP_BOTTOM = 20.0
@@ -16,6 +16,7 @@ class Stage(gtk.DrawingArea):
     gtk.DrawingArea.__init__(self)
 
     self.color_grade = color_grade
+    self.blocks = []
     self.connect("expose_event", self.expose)
 
   def expose(self, widget, event):
@@ -32,7 +33,7 @@ class Stage(gtk.DrawingArea):
     drawable.draw_rectangle(gc, True, block.px, block.py, BLOCK_WD, BLOCK_HG)
 
   def draw_all(self):
-    bks = blocks.generate_block_list(
+    self.blocks = blocks.generate_block_list(
             TOTAL_LINES, 
             TOTAL_COLUMNS, 
             BLOCK_TEMP_TOP, 
@@ -45,12 +46,30 @@ class Stage(gtk.DrawingArea):
 
     for i in range(TOTAL_LINES):
       for j in range(TOTAL_COLUMNS):
-        self.draw(bks[i][j])
+        self.draw(self.blocks[i][j])
+
+    gtk.timeout_add(1000, self.calculate)
+
+  def calculate(self):
+    for i in range(1, TOTAL_LINES-1):
+      for j in range(1, TOTAL_COLUMNS-1):
+        temp = self.blocks[i][j].temp
+        t = self.blocks[i-1][j].temp
+        t += self.blocks[i][j+1].temp
+        t += self.blocks[i+1][j].temp
+        t += self.blocks[i][j-1].temp
+        t *= 0.25
+
+        if temp != t:
+          self.blocks[i][j].temp = t
+          self.draw(self.blocks[i][j])
+
+    gtk.timeout_add(1, self.calculate) # 1000/60
 
 
 if __name__ == "__main__":
   window = gtk.Window()
-  window.set_size_request(800, 600)
+  window.set_size_request(800, 640)
 
   stg = Stage(max(BLOCK_TEMP_TOP, BLOCK_TEMP_RIGHT, BLOCK_TEMP_BOTTOM, BLOCK_TEMP_LEFT)/64.0)
   window.add(stg)
